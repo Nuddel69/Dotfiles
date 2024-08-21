9dc84942042799c6954f2b967aaac1efa7db4cc6
Nuddel69 <mats.bjonnes@gmail.com>
2024-08-21 17:44:20 +0200

    Configured Fish

---
+++ .config/fish/conf.d/alias.fish
@@ -0,0 +1,40 @@
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Changing "ls" to "eza"
+alias ls='eza -al --color=always --icons --group-directories-first' # my preferred listing
+alias la='eza -a --color=always --group-directories-first' # all files and dirs
+alias ll='eza -l --color=always --group-directories-first' # long format
+alias lt='eza -aT --color=always --group-directories-first' # tree listing
+alias l.='eza -a | egrep "^\."'
+
+# adding flags
+alias df='df -h' # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias tag='git tag'
+alias newtag='git tag -a'
+
+# Tmux
+
+# Vim
+alias vim nvim
